5b753887a8e1a64b468d4046eaceeae442b017dd
charulagrl <charul.agrl@gmail.com>
2018-03-27 01:56:21 +0530

    Data store to keep all the customer and office objects

---
+++ closest_customers/store/data_store.py
@@ -0,0 +1,16 @@
+class DataStore(object):
+	def __init__(self):
+		self.customers = {}
+		self.offices = {}
+
+	def add_customer(self, customer):
+		if not self.customers.get(customer.id):
+			self.customers[customer.id] = customer
+		else:
+			print("ERROR: customer id already exists")
+
+	def add_offices(self, office):
+		if not self.offices.get(office.id):
+			self.customers[office.id] = office
+		else:
+			print("ERROR: office id already exists")
