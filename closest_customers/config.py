8cc74674991240bd8ea2666c39d228575ace22d2
charulagrl <charul.agrl@gmail.com>
2018-03-29 00:26:13 +0530

    Add two environments for config

--- closest_customers/config.py
+++ closest_customers/config.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+
 class Config(object):
 	''' Class to keep the constant variables'''
-	path = 'data.json'
-	
-	limit = 100
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class Production(Config):
+	data_file_path = os.path.join(Config.dir_path, 'data.json')
+	distance_limit = 100
+
+
+class Staging(Config):
+	data_file_path = os.path.join(Config.dir_path, 'test_data.json')
+	distance_limit = 10
